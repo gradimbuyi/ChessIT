8010452dd0f12383496a7c3e5cbb2ab5b451a2fc
Gradi Mbuyi <gradimbuyi@outlook.com>
2026-02-28 22:24:41 -0500

    clean up

--- src/move.h
+++ src/move.h
@@ -12,23 +12,24 @@ class Move {
     protected:
         unsigned int m_Move;
 
-    Move(unsigned int from, unsigned int to, unsigned int flags) {
-        m_Move = ((flags & 0xf) << 12) | ((from & 0x3f) << 6) | (to & 0x3f);
-    }
+    public:
+        Move(unsigned int from, unsigned int to, unsigned int flags) {
+            m_Move = ((flags & 0xf) << 12) | ((from & 0x3f) << 6) | (to & 0x3f);
+        }
 
-    unsigned int getTo()   { return m_Move & 0x3f; }
-    unsigned int getFrom() { return (m_Move >> 6) & 0x3f; }
-    unsigned int getFlag() { return (m_Move >> 12) & 0x0f; }
+        unsigned int getTo()   { return m_Move & 0x3f; }
+        unsigned int getFrom() { return (m_Move >> 6) & 0x3f; }
+        unsigned int getFlag() { return (m_Move >> 12) & 0x0f; }
 
-    void setTo(unsigned int to) { m_Move &= ~0x3f; m_Move |= to & 0x3f; }
-    void setFrom(unsigned int from) { m_Move &= ~0xfc0; m_Move |= (from & 0x3f) << 6; }
-    bool isCapture() const { return (m_Move & CAPTURES) != 0; }
+        void setTo(unsigned int to) { m_Move &= ~0x3f; m_Move |= to & 0x3f; }
+        void setFrom(unsigned int from) { m_Move &= ~0xfc0; m_Move |= (from & 0x3f) << 6; }
+        bool isCapture() const { return (m_Move & CAPTURES) != 0; }
     
-    unsigned int getButterflyIndex() const { return m_Move & 0x0fff; } 
+        unsigned int getButterflyIndex() const { return m_Move & 0x0fff; } 
 
-    bool operator==(Move a) const { return (m_Move & 0xffff) == (a.m_Move & 0xffff); }
-    bool operator!=(Move a) const { return (m_Move & 0xffff) != (a.m_Move & 0xffff); }
-    void operator=(Move a) { m_Move = a.m_Move; }    
+        bool operator==(Move a) const { return (m_Move & 0xffff) == (a.m_Move & 0xffff); }
+        bool operator!=(Move a) const { return (m_Move & 0xffff) != (a.m_Move & 0xffff); }
+        void operator=(Move a) { m_Move = a.m_Move; }    
 };
 
 #endif
